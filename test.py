595f2e57f84c2206bd98383d23d61f5b5d19a71f
franksnightmare <chnstone13backup@gmail.com>
2018-09-13 18:16:46 +0200

    it works

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import sys
+import re
 from googlesearch.googlesearch import GoogleSearch
-
-def cleanWord(word):
-	return word.strip("\n").strip("\t")
 
 def getKeyword(stringList, skip = 1):
 	strings = len(stringList)
@@ -23,86 +21,29 @@ def main():
 	
 	print(keyword)
 	
-	try:
-		response = GoogleSearch().search("site:procon.org " + keyword, num_results = 1)
-		skipStep = 0
-		startStep = 0
-		start = False
-		end = False
+	response = GoogleSearch().search("site:procon.org " + keyword, num_results = 1)
+	arguments = {"pro":[], "con":[]}
+	
+	# Work in progress and also a mess
+	# On top of that either google or procon.org does not like the scraping and maybe blocked me once?
+	# 
+	for result in response.results:
+		print(("Title: " + result.title).encode(sys.stdout.encoding, errors='replace'))
+		content = ''.join(repr(result.getMarkup()).split('\n'))
 		
-		arguments = 0
-		argumentList = []
+		proBlock = re.findall(r'pro-quote-box">(.+?)</div', content)
 		
-		# Work in progress and also a mess
-		# On top of that either google or procon.org does not like the scraping and maybe blocked me once?
-		# 
-		for result in response.results:
-			print(("Title: " + result.title).encode(sys.stdout.encoding, errors='replace'))
+		for case in proBlock:
+			arguments["pro"].append(re.findall(r'bolded-intro">(.+?)</span', case))
 			
-			for word in result.getText().split(" "):
-				if (word):
-					someWord = cleanWord(word)
-					
-					# Stop if the end is reached (did you know section)
-					if skipStep >= 3:
-						break
-					
-					# Finding the start of an argument
-					if (not start and (someWord == "Pro" or someWord == "Con")):
-						startStep = 1
-						continue
-					
-					if startStep == 1:
-						if word.isnumeric():
-							startStep = 0
-							start = True
-							end = False
-							startSkip = 2
-							argumentList.append("")
-						else:
-							startStep = 0
-					
-					# Parsing the argument
-					if start:
-						if startSkip:
-							startSkip -= 1
-							continue
-						
-						if not end:
-							print(arguments)
-							print(someWord.encode(sys.stdout.encoding, errors='replace'))
-							argumentList[arguments] += someWord.encode(sys.stdout.encoding, errors='replace') + " "
-						
-						# The first sentence should be all of the argument we want right now
-						if "." in someWord:
-							end = True
-							arguments += 1
-					
-					# Should be the end of all the arguments (MAYBE)
-					# I would check but since I can't do any requests anymore...
-					# Only stop if any arguments were found
-					if arguments != 0:
-						if someWord == "Did":
-							skipStep += 1
-							continue
-						if skipStep == 1:
-							if someWord == "You":
-								skipStep += 1
-								continue
-							else:
-								skipStep = 0
-						if skipStep == 2:
-							if someWord == "Know?":
-								skipStep += 1
-								continue
-							else:
-								skipStep = 0
-			
-			# We are only interested in the first result right now
-			break
-	except HTTPError as httpError:
-		print("WE MESSED UP")
-		raise
+		conBlock = re.findall(r'con-quote-box">(.+?)</div', content)
+		for case in conBlock:
+			arguments["con"].append(re.findall(r'bolded-intro">(.+?)</span', case))
+		
+		# print(word.encode(sys.stdout.encoding, errors='replace'))
+		break
+	
+	print(arguments)
 
 if __name__ == "__main__":
 	main()
